562f377c6481d2406c2bf7d6b33cb2822cdd2c5f
CathyCahy <cathycahy@gmail.com>
2024-03-03 16:48:50 +0300

    homework

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         // задача 1
-        var dog = 8;
+        var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
